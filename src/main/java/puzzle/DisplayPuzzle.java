1de4aab94221587f01136202c32fe8611e01db4b
Douglas Luman <dluman@douglasjluman.com>
2019-09-13 09:22:01 -0400

    Adding more build done in the 'production' environment.

--- src/main/java/puzzle/DisplayPuzzle.java
+++ src/main/java/puzzle/DisplayPuzzle.java
@@ -2,7 +2,7 @@ import puzzle.Pieces;
 
 /** This class displays the assembled puzzle.
  *
- * @author {YOUR NAME HERE}
+ * @author Douglas Luman
  */
 public class DisplayPuzzle {
   /** The main entry point for DisplayPuzzle.
@@ -10,10 +10,15 @@ public class DisplayPuzzle {
    * @param args The command line arguments
    */
   public static void main(String[] args) {
-    System.out.println(Pieces.hat());
-    System.out.println(Pieces.body());
-    System.out.println(Pieces.tailAndLegs());
-    System.out.println("");
-    System.out.print(" THE GATOR WIZARD STRIKES BACK!");
+    // These are string objects representing each puzzle piece.
+    String hat = Pieces.hat();
+    String body = Pieces.body();
+    String tailAndLegs = Pieces.tailAndLegs();
+    // These are the print statements that display the caption and puzzle pieces.
+    System.out.println(hat);
+    System.out.println(body);
+    System.out.println(tailAndLegs);
+    System.out.println();
+    System.out.print("      THE GATOR WIZARD STRIKES BACK!      ");
   }
 }
